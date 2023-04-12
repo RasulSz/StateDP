a7f4bbd129d68ee7b88ab966f9ec02b0054c13f7
XSbzi_uh51 <XSbzi_uh51@itstep.edu.az>
2023-04-12 17:01:30 +0400

    Add project files.

---
+++ StateDP/ClickButtons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateDP
+{
+    public interface IClickButtons
+    {
+        public void Click2();
+        public void Click4();
+        public void Click6();
+        public void Click8();
+        public void Click0();
+    }
+}
---
+++ StateDP/Program.cs
@@ -0,0 +1,14 @@
+﻿using StateDP;
+
+IClickButtons click = new WithNumLock();
+click.Click0();
+click.Click2();
+click.Click4();
+click.Click6();
+click.Click8(); 
+click=new WithOutNumLock();
+click.Click0();
+click.Click2();
+click.Click4();
+click.Click6();
+click.Click8();
